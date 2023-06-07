79503978cf7292f6f2f3c0aff4734f719dd05654
Priyansh Rai <priyanshrai16@gmail.com>
2023-06-07 10:22:08 +0530

    Added hover active state to cards and added redirection functionality

--- src/components/ShowCard/ShowCard.jsx
+++ src/components/ShowCard/ShowCard.jsx
@@ -5,34 +5,38 @@ import "./ShowCard.css";
 export const ShowCard = (props) => {
     console.log(props);
     return (
-        <div className="showcard">
-            <img
-                className="card-image"
-                src={props.show.image.original}
-                alt="Card Image"
-            />
-            <div className="card-text-container">
-                <div className="flex-row">
-                    <h3 className="card-title">{props.show.name}</h3>
-                    <span className="card-score">
-                        {Math.round(props.score * 100)}%
-                    </span>
-                </div>
-                <div>
-                    <span className="card-runtime">
-                        {props.show.runtime && props.show.runtime + "min | "}
-                    </span>
-                    <span className="card-genres">
-                        {props.show.genres
-                            .slice(0, 2)
-                            .map((genre, index, genres) => {
-                                return index + 1 !== genres.length
-                                    ? genre + ", "
-                                    : genre;
-                            })}
-                    </span>
+        <Link to={`/show/${props.show.id}`}>
+            <div className="showcard">
+                <button className="details-button">Details</button>
+                <img
+                    className="card-image"
+                    src={props.show.image.original}
+                    alt="Card Image"
+                />
+                <div className="card-text-container">
+                    <div className="flex-row">
+                        <h3 className="card-title">{props.show.name}</h3>
+                        <span className="card-score">
+                            {Math.round(props.score * 100)}%
+                        </span>
+                    </div>
+                    <div>
+                        <span className="card-runtime">
+                            {props.show.runtime &&
+                                props.show.runtime + "min | "}
+                        </span>
+                        <span className="card-genres">
+                            {props.show.genres
+                                .slice(0, 2)
+                                .map((genre, index, genres) => {
+                                    return index + 1 !== genres.length
+                                        ? genre + ", "
+                                        : genre;
+                                })}
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
